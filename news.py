459dfd6e6c8e9a9bedfea53f4d194d3f3a284bea
Alena <shohaalena77@gmail.com>
2022-07-05 20:39:26 +0300

    add name logic to news.py

--- news.py
+++ news.py
@@ -1,2 +1,14 @@
-a = 5+1
-print(a)
+my_list = [42, 43]
+my_dict = {
+    'foo': {'a':12,
+            'b': (1, 2, 3, 4, my_list)
+            },
+    'bar':{'c':12,
+           'd':{5, 6, 7, 8}
+           },
+    'moo':{'e':12,
+           'f':{'Lol':['L', 'o', 'l']}}}
+print('1.', my_dict['foo'])
+print('2.', my_dict['foo']['b'])
+my_list.append(44)
+print('3.', my_list)
